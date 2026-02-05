1b163ecc07080f62b90d84d46daafb61faaa1dbc
hong1350 <hong1350602@outlook.com>
2026-02-05 11:17:11 +0800

    对上一次提交的代码进行更正

--- qol.sh
+++ qol.sh
@@ -5,20 +5,12 @@ function check() {
     echo "正在检查缓存是否存在……"
     sleep 1
     #如果没有缓存则创建缓存
-    if [ ! -d ~/.cache/qol ]; then
+    if [ ! -f ~/.cache/qol/cache ]; then
 	echo "未找到缓存，即将创建新的缓存文件……"
         mkdir ~/.cache/qol
-	case QOL_PM in
-            (* | "未知")
-	esac
 	echo "您的软件包管理器：${QOL_PM}"
 	echo "QOL_PM = ${QOL_PM}" >> ~/.cache/qol/cache
 	source /etc/os-release
-	QOL_DISTRO=${PRETTY_NAME}
-	case QOL_DISTRO in
-	    ("Kali*" | "Kali")
-	    (* | "未知")
-	esac
 	echo "您使用的发行版：${QOL_DISTRO}"
 	echo "QOL_DISTRO = ${QOL_DISTRO}" >> ~/.cache/qol/cache
     fi
